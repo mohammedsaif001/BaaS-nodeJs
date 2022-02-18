4f328532fde48152eb7ed47ed8a8c465bb5f83ec
mohammedsaif001 <mdsaif4online@gmail.com>
2022-02-18 23:44:38 +0530

    Error Handling & Callbacks

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ var rect = require('./rectangle')
 function solveRect(l,b){
     console.log(`Solving for ${l} & ${b}`)
 
-    if(l<=0 || b<=0){
-        console.log("Rectangle dimensions should be greater than 0")
-    }
-    else{
-        console.log(`The area of the rectangle is ${rect.area(l,b)}`)
-        console.log(`The Perimeter of the rectangle is ${rect.perimeter(l,b)}`)
-    }
+   rect(l,b,(err,rectangle)=>{
+       if(err){
+           console.log(`Error: ${err.message}`)
+       }
+       else{
+           console.log(`The Area of the rectangle with the dimensions ${l} & ${b} is ${rectangle.area()}`)
+           console.log(`The Perimeter of the rectangle with the dimensions ${l} & ${b} is ${rectangle.perimeter()}`)
+       }
+   })
+   console.log("\nCompleted the Function\n")
 }
 
 solveRect(10,20)
+solveRect(0,20)
 solveRect(1,2)
